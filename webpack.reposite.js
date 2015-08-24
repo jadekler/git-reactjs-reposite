6965bd34eaf965b3672ad2542dcf5a418443505a
Jean de Klerk <jadekler@gmail.com>
2015-08-24 12:30:08 -0600

    Fixed jquery issue, although still seeing some console errors

--- webpack.reposite.js
+++ webpack.reposite.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const config = {
   output: {filename: 'generated-app.js'},
-  externals: {jquery: 'var jQuery'},
   context: __dirname,
   entry: ['./assets/javascripts/App'],
   resolve: {
